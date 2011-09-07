6f500bb80bc276110e50105a00f4f2f1e7715cf7
Yves Raimond <yves.raimond@bbc.co.uk>
2011-09-07 19:47:24 +0100

    Some more tests

--- tests/rdf_space_tests.py
+++ tests/rdf_space_tests.py
@@ -1,25 +1,32 @@
 from nose.tools import *
-import numpy as np
-from numpy.testing import *
 import rdfspace
 from rdfspace.space import Space
 
 def test_init():
-    rdf_space = Space('tests/example.n3')
-    assert_equal(rdf_space._path_to_rdf, 'file:tests/example.n3')
-    assert_equal(rdf_space._format, 'ntriples')
-    assert_equal(rdf_space._property, 'http://www.w3.org/2004/02/skos/core#broader')
-    assert_equal(rdf_space._index, {
+    space = Space('tests/example.n3')
+    assert_equal(space._path_to_rdf, 'file:tests/example.n3')
+    assert_equal(space._format, 'ntriples')
+    assert_equal(space._property, 'http://www.w3.org/2004/02/skos/core#broader')
+    assert_equal(space._direct_parents, {
         'http://dbpedia.org/resource/Category:Categories_named_after_television_series': ['http://dbpedia.org/resource/Category:Foo'],
         'http://dbpedia.org/resource/Category:Star_Trek': [
-            'http://dbpedia.org/resource/Category:Foo', 
             'http://dbpedia.org/resource/Category:Categories_named_after_television_series',
         ],
         'http://dbpedia.org/resource/Category:Futurama': [
+            'http://dbpedia.org/resource/Category:Categories_named_after_television_series',
             'http://dbpedia.org/resource/Category:New_York_City_in_fiction', 
-            'http://dbpedia.org/resource/Category:Foo', 
-            'http://dbpedia.org/resource/Category:Categories_named_after_television_series'
         ],
-        'http://dbpedia.org/resource/Category:Foo': [],
-        'http://dbpedia.org/resource/Category:New_York_City_in_fiction': [],
     })
+
+def test_parents():
+    space = Space('tests/example.n3')
+    assert_equal(space.parents('http://dbpedia.org/resource/Category:Futurama'), [
+        'http://dbpedia.org/resource/Category:New_York_City_in_fiction',
+        'http://dbpedia.org/resource/Category:Foo',
+        'http://dbpedia.org/resource/Category:Categories_named_after_television_series',
+    ])
+    assert_equal(space.parents('http://dbpedia.org/resource/Category:Foo'), [])
+
+def test_distance():
+    space = Space('tests/example.n3')
+    assert_equal(space.distance('http://dbpedia.org/resource/Category:Futurama', 'http://dbpedia.org/resource/Category:Star_Trek'), 0.81649658092772592)
