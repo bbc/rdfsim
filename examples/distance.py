af8af25aa125ca166fb34488f20d8a413a1149d5
Yves Raimond <yves.raimond@bbc.co.uk>
2012-07-09 11:24:49 +0100

    Updating example

--- examples/distance.py
+++ examples/distance.py
@@ -1,10 +1,10 @@
 import rdfsim
 from rdfsim.space import Space
 
-space = Space('skos_categories_1000.nt', ignored_predicates=['http://www.w3.org/1999/02/22-rdf-syntax-ns#type'])
+space = Space('skos_categories_1000.nt', property='http://www.w3.org/2004/02/skos/core#broader')
 
 print "Distance betwen Dwarves and Elves:"
-print space.distance('http://dbpedia.org/resource/Category:Middle-earth_Dwarves', 'http://dbpedia.org/resource/Category:Middle-earth_Elves')
+print space.similarity_uri('http://dbpedia.org/resource/Category:Middle-earth_Dwarves', 'http://dbpedia.org/resource/Category:Middle-earth_Elves')
 
 print "Distance between Dwarves and Futurama:"
-print space.distance('http://dbpedia.org/resource/Category:Middle-earth_Dwarves', 'http://dbpedia.org/resource/Category:Futurama')
+print space.similarity_uri('http://dbpedia.org/resource/Category:Middle-earth_Dwarves', 'http://dbpedia.org/resource/Category:Futurama')
